8603402f68fd75362562ce83dec76940d628e2a9
La-Keisha <LTowner622@gmail.com>
2016-11-26 18:47:16 -0500

    Added .each function and .map functions to the the array

---
+++ ruby/iteration.rb
@@ -0,0 +1,43 @@
+#declare an array and a hash, and populate each of them with some data. 
+
+dog_name = []
+#declaring an empty array
+dog_name = %W[Ashley Tom Bob Danielle]
+
+p dog_name
+#printing the array
+
+dog_information = Hash.new 
+#declaring an empty hash
+dog_information [:Ashley] = 6
+dog_information[:Tom] = 1
+dog_information[:Bob] = 3
+dog_information[:Danielle] = 2
+
+#could also declare hash like this:
+#dog_information = {Ashley => 6,Tom =>1, Bob => 3, Danielle => 2}
+
+p dog_information
+#printing the hash
+
+#Demonstrate that you can iterate through each one 
+#using .each, and then using .map! (modifying the data in some way).
+#Note that you can't call .map! on a hash, so you can skip that 
+#(.map returns an array, so a destructive map method is logically impossible on a hash). 
+#Print the data structures before and after each call to demonstrate whether they 
+#have been modified or not.
+
+
+#iteration through the hash using .each
+dog_name.each do |name| puts "The dog's name is #{name}"
+end
+
+p dog_name
+#priting before the .map call
+
+#iteration through the hash using .map
+dog_name.map! do |x|
+	x.delete("o")
+end
+#.map! deletes any instances of the letter 'o' if found from the name
+p dog_name
