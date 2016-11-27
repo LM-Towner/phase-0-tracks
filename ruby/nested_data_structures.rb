045c7193462ff0527f6574b9d164f4a7f89a0da4
La-Keisha <LTowner622@gmail.com>
2016-11-26 21:30:00 -0500

    describe the nested data structure that will be created, and all of the components that will go in

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,14 @@
+=begin 
+
+In this nested data structure, I will be creating Hash element named Amusement park which will
+feature different components you may find in an amusement park.
+
+Visual representation of nested data structures:
+
+Amusement Park
+Ride -> Roller Coaster, Ferris Wheel, Haunted House
+Arcade -> Video Games, Bowling, Prizes
+Concession Stand -> Hamburgers, French Fries.  Pizza, Cotton Candy
+People -> Adults, Children, Teenager
+
+=end 
