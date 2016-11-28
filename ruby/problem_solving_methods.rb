d1bc0193007d457991ec9e482b6c1ead89142889
La-Keisha <LTowner622@gmail.com>
2016-11-28 00:17:39 -0500

    added the the fib. sequence to the file allow with comments

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -24,3 +24,36 @@ def take_array(array, integer)
   end
 end
 p take_array(arr,1)
+
+
+#Release 1: Calculate Fibonacci Numbers 
+
+=begin
+The concept of Fibonacci numbers is pretty simple: you start off with a 0 and a 1, 
+and keep adding numbers onto the list by adding the previous two. 
+With that in mind, an array of the first six Fibonacci terms would be [0,1,1,2,3,5]. 
+Each number in the array is the sum of the previous two numbers, except for the 0 and the 1, 
+which we started out with.(Note that some Fibonacci sequences start out with two 1s instead. Don't let that confuse you.)	
+=end
+
+def fib(integer)
+
+fib_arr = [0,1]
+#declaring the beginning portion of the fibanocci sequence
+index = 2
+#starting at two because the array is already filled with the 0 & 1 index's filled 
+	until index == integer 
+# the integer will be the fib(x) for the sequence
+	next_number = fib_arr[-1] + fib_arr[-2]
+#the next number in the sequence will equal the last number in the array, represented by -1, and the second to
+#last number represented by -2 added together.
+	fib_arr << next_number
+#feeding the next_number into the array
+	index += 1
+#changing to the next index in the array
+	end
+	puts fib_arr
+#printing the new array
+end
+
+fib(100)
