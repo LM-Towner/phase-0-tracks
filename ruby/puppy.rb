bb479ceb6631a61adcd46807b983dd6b65c1fc65
La-Keisha <LTowner622@gmail.com>
2016-11-28 23:41:26 -0500

    added a class named 'Puppy' with attributes and proposed methods for the class

---
+++ ruby/puppy.rb
@@ -0,0 +1,31 @@
+class Puppy
+
+=begin
+	
+Puppies Method 
+
+----------------Breed------------
+Yorkshire Terrier
+
+
+------------Characteristics------------	
+
+Adapts Well to Apartment Living: 5 Stars
+Good For Novice Owners : 5 Stars
+Tolerates Being Alone : 2 Stars
+Size : 1 Star
+Energy Level : 5 Stars
+
+------------Behavior------------	
+Very Active
+Can be loud 
+Tendency to bite, also known as "nipping"
+Very Sensitive
+
+
+All information taken from :
+http://dogtime.com/dog-breeds/yorkshire-terrier#/slide/1
+
+	
+=end
+end
