676e81c4967f4e31f9777093982782d1f5a2c40d
La-Keisha <LTowner622@gmail.com>
2016-12-10 12:06:14 -0500

    Changed the name of file and completed gps file

--- ruby/gps6/solution.rb
+++ ruby/gps6/solution.rb
@@ -4,44 +4,59 @@
 # We spent [#] hours on this challenge.
 
 # EXPLANATION OF require_relative
-#
+#Require Relative is using a file within the same directory without using the file extension
 #
 require_relative 'state_data'
 
 class VirusPredictor
 
+	#just initializing the data for the class for each new instance	
   def initialize(state_of_origin, population_density, population)
     @state = state_of_origin
     @population = population
     @population_density = population_density
   end
 
+  #method is calling the other methods predicted_deaths & speed of spread
   def virus_effects
-    predicted_deaths(@population_density, @population, @state)
-    speed_of_spread(@population_density, @state)
+    predicted_deaths
+    #predicted_deaths(@population_density, @population, @state)
+    #speed_of_spread(@population_density, @state)
+    speed_of_spread
   end
 
   private
 
-  def predicted_deaths(population_density, population, state)
+  #prediction of deaths of the population and state 
+  #def predicted_deaths(population_density, population, state)
+  def predicted_deaths
     # predicted deaths is solely based on population density
+    num = 1
+    prediction = number_of_deaths =(@population  * num).floor
     if @population_density >= 200
-      number_of_deaths = (@population * 0.4).floor
+    	num = 0.4
+    #number_of_deaths = (@population * 0.4).floor
     elsif @population_density >= 150
-      number_of_deaths = (@population * 0.3).floor
+      #number_of_deaths = (@population * 0.3).floor
+      num = 0.3
     elsif @population_density >= 100
-      number_of_deaths = (@population * 0.2).floor
+      #number_of_deaths = (@population * 0.2).floor
+      num = 0.2
     elsif @population_density >= 50
-      number_of_deaths = (@population * 0.1).floor
+      #number_of_deaths = (@population * 0.1).floor
+      num = 0.1
     else
-      number_of_deaths = (@population * 0.05).floor
+      #number_of_deaths = (@population * 0.05).floor
+      num = 0.05
     end
 
     print "#{@state} will lose #{number_of_deaths} people in this outbreak"
 
   end
 
-  def speed_of_spread(population_density, state) #in months
+  #determining of fast the virus will spread
+  def speed_of_spread
+  #def speed_of_spread(population_density, state) #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
     speed = 0.0
@@ -83,5 +98,21 @@ alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density],
 alaska.virus_effects
 
 
+def all_states(states)
+	states.each do |state,population| 
+		projection = VirusPredictor.new(state,population[:population_density],population[:population])
+		projection.virus_effects
+	end
+end
+
+all_states(STATE_DATA)
+
+
+
 #=======================================================================
 # Reflection Section
+#The two different hashe syntaxes shown in the state_data file are the hashes and symbols
+#Require_relative is used to refer to a file within the same directory. Require can be used to find a file somewhere on your computer. Like a library for your program
+#You can iterate through a hash using .each or .map. 
+#When refactoring virus_effects, I realized that they were using variables that did not need to be used since it was already declared in the initialize method, all it need to do was call the method and use the variables within the method.
+#I learned what to look for when refactoring methods, and what all CAPS for declaring variable means.
