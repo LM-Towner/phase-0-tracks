d48898c7b45294f5aa79855b72ad2129ba3f8749
La-Keisha <LTowner622@gmail.com>
2016-12-10 15:19:25 -0500

    Wrote some pseudocode

---
+++ js/explore.js
@@ -0,0 +1,13 @@
+/*
+		 Psuedocode for a reverse method for JavaScript
+-> Create a method that will take a string
+-> For each letter within the string of the program will read and manipulate them seperately
+-> Once each character within the string is seperated, the program will store the string as seperated characters as a new string
+-> The program will identify how long the strin is and identify which character is the terminal one
+-> Once identifyed the length as well as how many characters are within the string,the program will now take the last character and make it first
+-> The program will now do this until the first character is now the last character
+-> After all the characters changes their position within the string, the console will now print the modified string.
+
+*/
+
+
