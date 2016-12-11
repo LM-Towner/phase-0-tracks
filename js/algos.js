43726dca77b9805b26ed3d5b051497b1a8f3ee68
La-Keisha <LTowner622@gmail.com>
2016-12-10 21:41:48 -0500

    Added test for the key value function

--- js/algos.js
+++ js/algos.js
@@ -13,7 +13,7 @@ function longestStringHere(findString){
 
 // Release 1 : Find a key-value match pair
 
-function kVMAtch(obj1, obj2){
+function kVMatch(obj1, obj2){
 	for (var key in obj1){
 		if(obj1[key] == obj2[key]){
 			return true;
@@ -22,12 +22,28 @@ function kVMAtch(obj1, obj2){
 	return false;
 }
 
-
-
-
-//_______________________________________________________
-//driver code to test function longestString here
-
+/*______________________________________________________
+	driver code to test function longestString here
+________________________________________________________*/
 console.log(longestStringHere(["this","may","be","it"]));
 console.log(longestStringHere(["abc","defgh","be","i"]));
 console.log(longestStringHere(["well","seeee","aboouuuuttt","it"]));
+
+/*______________________________________________________
+	driver code to test function for match key function
+________________________________________________________*/
+
+var obj1 = {name: "Steven",  age: 54};
+var obj2 = {name: "Tamir",   age: 54};
+var obj3 = {name: "Jocelyn", age: 1};
+var obj4 = {name: "Kim",     age: 2};
+var obj5 = {name: "John",    age: 26};
+var obj6 = {name: "John",    age: 25};
+console.log(kVMatch(obj1, obj2)); 
+console.log(kVMatch(obj1, obj3)); 
+console.log(kVMatch(obj2, obj4)); 
+console.log(kVMatch(obj3, obj4));
+console.log(kVMatch(obj6, obj5));
+
+
+
